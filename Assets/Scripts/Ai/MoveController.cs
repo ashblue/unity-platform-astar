26ed048c57a70352cb3a4311ccb2a5f254ebb804
Ash Blue <ash@blueashes.com>
2015-02-01 04:11:28 -0800

    Added new AI move mechanism that tracks feet pos

--- Assets/Scripts/Ai/MoveController.cs
+++ Assets/Scripts/Ai/MoveController.cs
@@ -7,4 +7,11 @@ public class MoveController : MonoBehaviour {
 	public void SimpleMove (Vector3 dir) {
 		transform.position += dir;
 	}
+
+	// Moves the character, but keeps the y axis the same
+	public void FeetMove (Vector3 dir) {
+//		float prevY = transform.position.y;
+		dir.y = 0f;
+		transform.position += dir;
+	}
 }
